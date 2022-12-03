b89034fff6b3879c3facf939afa604e4c56694cc
mustpe <mustpe@gmail.com>
2022-12-04 02:15:19 +0800

    style time selector

--- components/SelectStation.tsx
+++ components/SelectStation.tsx
@@ -1,10 +1,22 @@
 import { Ionicons, SimpleLineIcons } from '@expo/vector-icons';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import { VStack, HStack, Box, Center, Text, Icon, Button, Input, Pressable } from 'native-base';
+import {
+  VStack,
+  HStack,
+  Box,
+  Center,
+  Text,
+  Icon,
+  Button,
+  Input,
+  Pressable,
+  Divider,
+  Flex,
+} from 'native-base';
 import React, { useState } from 'react';
 
 export default function SelectStation(): JSX.Element {
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date(1668165071));
   const [destination, setDestination] = useState('台北');
   const [departure, setDeparture] = useState('中壢');
   const handleSwapDepartureAndDestination = () => {
@@ -79,9 +91,27 @@ export default function SelectStation(): JSX.Element {
         </Box>
       </HStack>
       <Box mt="4" bg="info.900" w="300" h="50" rounded="xl">
-        <Input />
+        <Flex direction="row" h="50">
+          <Center
+            flex="3"
+            _text={{
+              fontSize: '2xl',
+              color: 'white',
+            }}>
+            2022/11/11
+          </Center>
+          <Divider bg="#0A1E45" orientation="vertical" />
+          <Center
+            flex="2"
+            _text={{
+              fontSize: '2xl',
+              color: 'white',
+            }}>
+            11:11
+          </Center>
+        </Flex>
       </Box>
-      <Button mt="5" width="150">
+      <Button mt="5" width="150" rounded="3xl">
         <HStack space={2} alignItems="center">
           <Icon as={SimpleLineIcons} name="magnifier" size={4} color="white" mt="0.5" />
           <Text fontSize="md">查詢時刻</Text>
